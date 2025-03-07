0d205dbff9faaeb74a3c9a8a34393f3c5ea494c7
unknown <gaviriayeferson2@gmail.com>
2025-03-07 14:02:56 +0100

    Initial commit projects setup

---
+++ src/main/java/com/f5/commit_or_die/CommitOrDieApplication.java
@@ -0,0 +1,13 @@
+package com.f5.commit_or_die;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommitOrDieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommitOrDieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/f5/commit_or_die/CommitOrDieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.f5.commit_or_die;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommitOrDieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
