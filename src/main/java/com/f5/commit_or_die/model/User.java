32cc4223e43f8c3a6f77e762573d2acbc20e9987
unknown <gaviriayeferson2@gmail.com>
2025-03-09 18:56:05 +0100

    Add getters and setters for User entity

--- src/main/java/com/f5/commit_or_die/model/User.java
+++ src/main/java/com/f5/commit_or_die/model/User.java
@@ -21,5 +21,67 @@ public class User {
     
     private String role;
     
-   
+    public User() {
+    }
+
+    public User(Long id, String name, String email, String password, Date registrationDate, String role) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.registrationDate = registrationDate;
+        this.role = role;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Date getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setRegistrationDate(Date registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    
+
+    
 }
