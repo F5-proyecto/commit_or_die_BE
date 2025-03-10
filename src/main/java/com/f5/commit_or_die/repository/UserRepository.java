caeda1bd6bd3c21bbe965d06d4c407534a7d12f1
unknown <gaviriayeferson2@gmail.com>
2025-03-10 17:16:06 +0100

    Created UserRepository interface with custom method findByEmail using Spring Data JPA

---
+++ src/main/java/com/f5/commit_or_die/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.f5.commit_or_die.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.f5.commit_or_die.model.User;
+
+public interface UserRepository  extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+    
+}
