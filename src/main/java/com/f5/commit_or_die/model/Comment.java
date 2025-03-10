f4c821d5ab709e0bb753e3443746ba8b043c1b1c
Celia Garrido <celiagarridoherrera@gmail.com>
2025-03-10 11:27:02 +0100

    implement getters and setters for Comment (model)

--- src/main/java/com/f5/commit_or_die/model/Comment.java
+++ src/main/java/com/f5/commit_or_die/model/Comment.java
@@ -1,7 +1,6 @@
 package com.f5.commit_or_die.model;
 
-import java.sql.Date;
-
+import java.util.Date;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -9,6 +8,8 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 
 @Entity
 @Table(name = "comments")
@@ -19,13 +20,75 @@ public class Comment {
     private Long id;
 
     private String content;
+
+    @Temporal(TemporalType.TIMESTAMP)
     private Date publicationDate;
+    
     private Integer rating;
     private String resourceId;
      
     @ManyToOne
     @JoinColumn(name = "user_id")
-    private User user;   
+    private User user;
+
+    public Comment() {}
+
+    public Comment(Long id, String content, Date publicationDate, Integer rating, String resourceId, User user) {
+        this.id = id;
+        this.content = content;
+        this.publicationDate = publicationDate;
+        this.rating = rating;
+        this.resourceId = resourceId;
+        this.user = user;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getPublicationDate() {
+        return publicationDate;
+    }
+
+    public void setPublicationDate(Date publicationDate) {
+        this.publicationDate = publicationDate;
+    }
+
+    public Integer getRating() {
+        return rating;
+    }
+
+    public void setRating(Integer rating) {
+        this.rating = rating;
+    }
+
+    public String getResourceId() {
+        return resourceId;
+    }
+
+    public void setResourceId(String resourceId) {
+        this.resourceId = resourceId;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }   
 
 
 }
