590c2b775a256bb64db3c8cf1e899d9963763a7c
Celia Garrido <celiagarridoherrera@gmail.com>
2025-03-10 11:28:47 +0100

    reduce imports for jakarta.persitance

--- src/main/java/com/f5/commit_or_die/model/Comment.java
+++ src/main/java/com/f5/commit_or_die/model/Comment.java
@@ -1,15 +1,7 @@
 package com.f5.commit_or_die.model;
 
 import java.util.Date;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
-import jakarta.persistence.Temporal;
-import jakarta.persistence.TemporalType;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "comments")
