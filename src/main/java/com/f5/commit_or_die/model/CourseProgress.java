e614c393e40cd62553587e4f86059a59a3b74e0d
Karina Lorenzo <karinalrg16@gmail.com>
2025-03-10 11:52:26 +0100

    reduce imports for jakarta.persistance

--- src/main/java/com/f5/commit_or_die/model/CourseProgress.java
+++ src/main/java/com/f5/commit_or_die/model/CourseProgress.java
@@ -1,10 +1,6 @@
 package com.f5.commit_or_die.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "course_progress")
