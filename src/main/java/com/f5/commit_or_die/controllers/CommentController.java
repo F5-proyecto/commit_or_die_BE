e98b40f8328e3b12749a1e7ac300ab212bb01750
Celia Garrido <celiagarridoherrera@gmail.com>
2025-03-11 23:38:39 +0100

    Create create/update/delete methods for CommentController

--- src/main/java/com/f5/commit_or_die/controllers/CommentController.java
+++ src/main/java/com/f5/commit_or_die/controllers/CommentController.java
@@ -6,8 +6,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.f5.commit_or_die.model.Comment;
 import com.f5.commit_or_die.services.CommentService;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
 
 
 @RestController
@@ -23,7 +30,19 @@ public class CommentController {
     public List<Comment> getComments(@PathVariable String resourceId) {
         return commentService.getComments(resourceId);
     }
-    
 
-    
+    @PostMapping("/comments")
+    public Comment createComment(@RequestBody Comment comment) {
+        return commentService.createComment(comment);
+    }
+
+    @PutMapping("comments")
+    public void updateComment( @RequestBody Comment comment) {
+        commentService.updateComment(comment);
+    }
+
+    @DeleteMapping("path/{id}")
+    public void deleteComment(@PathVariable Long id) {
+        commentService.deleteComment(id);
+    }
 }
