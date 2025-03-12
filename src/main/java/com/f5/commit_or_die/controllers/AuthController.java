80239dbcf20da9f5961437bd2644433ae6ed0f47
unknown <gaviriayeferson2@gmail.com>
2025-03-12 21:40:02 +0100

    Add AuthController to handle user authentication endpoints

--- src/main/java/com/f5/commit_or_die/controllers/AuthController.java
+++ src/main/java/com/f5/commit_or_die/controllers/AuthController.java
@@ -1,5 +1,40 @@
 package com.f5.commit_or_die.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.f5.commit_or_die.dto.AuthRequest;
+import com.f5.commit_or_die.services.AuthService;
+import com.f5.commit_or_die.model.User;
+
+@RestController
+@RequestMapping("/auth")
+
 public class AuthController {
 
+    private AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody AuthRequest authRequest) {
+        boolean isAuthenticated = authService.login(authRequest.getEmail(), authRequest.getPassword());
+        if (isAuthenticated) {
+            return ResponseEntity.ok("Login successful");
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials");
+        }
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<User> register(@RequestBody User user) {
+        User registeredUser = authService.register(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(registeredUser);
+    }
 }
